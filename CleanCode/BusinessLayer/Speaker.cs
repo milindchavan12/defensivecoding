7eb1fcee0b16415dc15c97be66e4fb7e641ff527
Milind Chavan <milind.chavan@live.com>
2019-03-25 16:49:03 +0100

    With Register

---
+++ CleanCode/BusinessLayer/Speaker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public class Speaker
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int YearExperience { get; set; }
+        public int HasBlogs { get; set; }
+        public string Employer { get; set; }
+
+        public int Register(IRepository repository)
+        {
+            ValidateData();
+
+            var isSpeakAppearedQualified = AppearsExceptional();
+        }
+
+        private void ValidateData()
+        {
+            if (string.IsNullOrEmpty(FirstName)) return ArgumentNullException("First name is required");
+            if (string.IsNullOrEmpty(LastName)) return ArgumentNullException("Last name is required");
+            if (string.IsNullOrEmpty(Email)) return ArgumentNullException("Email is required");
+        }
+
+        private bool AppearsExceptional()
+        {
+            if (YearExperience > 10) return true;
+            if (HasBlogs) return true;
+
+            var preffferdEmployer = new List<string>() { "PluralSight", "Microsoft", "Google" };
+            if (preffferdEmployer.Contains(Employer)) return true;
+
+            return false;
+        }
+    }
+}
